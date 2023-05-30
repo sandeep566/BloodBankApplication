d601876eb61668d0e3a1599df8e423f3997676be
sandeep566 <79628409+sandeep566@users.noreply.github.com>
2023-05-30 21:05:35 +0530

    update 2 BloodBank

--- src/main/java/com/yp/BloodBankApplication/Controller/BloodBankController.java
+++ src/main/java/com/yp/BloodBankApplication/Controller/BloodBankController.java
@@ -1,6 +1,8 @@
 package com.yp.BloodBankApplication.Controller;
 
 import com.yp.BloodBankApplication.Entity.BloodBank;
+import com.yp.BloodBankApplication.Entity.Donor;
+import com.yp.BloodBankApplication.Enums.BloodGroup;
 import com.yp.BloodBankApplication.Requests.BloodBankRequest;
 import com.yp.BloodBankApplication.Services.BloodBankService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Controller class for handling blood bank-related API endpoints.
+ */
 @RestController
 @RequestMapping("/bloodBank")
 public class BloodBankController {
@@ -15,15 +24,86 @@ public class BloodBankController {
     @Autowired
     public BloodBankService bloodBankService;
 
+    /**
+     * Adds a new blood bank.
+     *
+     * @param bloodBankRequest The details of the blood bank to be added.
+     * @return The added blood bank.
+     */
+
     @PostMapping("/add")
     public ResponseEntity<BloodBank> addBloodBank(@RequestBody BloodBankRequest bloodBankRequest){
         return new ResponseEntity<>(bloodBankService.registerBloodBank(bloodBankRequest), HttpStatus.CREATED);
     }
 
+    /**
+     * Updates an existing blood bank.
+     *
+     * @param bloodBank The updated details of the blood bank.
+     * @return The updated blood bank.
+     */
+
 
     @PutMapping("/update")
     public ResponseEntity<BloodBank> updateBloodBank(@RequestBody BloodBankRequest bloodBank){
         return new ResponseEntity<>(bloodBankService.updateBloodBank(bloodBank),HttpStatus.OK);
+    }
+
+    /**
+     * Retrieves a blood bank by its ID.
+     *
+     * @param id The ID of the blood bank to retrieve.
+     * @return The blood bank with the specified ID.
+     */
+
+    @GetMapping("/view/{id}")
+    public ResponseEntity<BloodBank> getBloodBank(@PathVariable int id){
+        return new ResponseEntity<>(bloodBankService.viewBloodBank(id),HttpStatus.FOUND);
+    }
+
+
+    /**
+     * Retrieves all blood banks.
+     *
+     * @return A list of all blood banks.
+     */
+    @GetMapping("/viewAll")
+    public ResponseEntity<List<BloodBank>> getAllBloodBanks(){
+        return new ResponseEntity<>(bloodBankService.viewAllBloodBanks(),HttpStatus.OK);
+    }
+
+    /**
+     * Deletes a blood bank by its ID.
+     *
+     * @param id The ID of the blood bank to delete.
+     * @return A message indicating the deletion status.
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> removeBloodBank(@PathVariable int id){
+        return new ResponseEntity<>(bloodBankService.deleteBloodBank(id),HttpStatus.OK);
+    }
+
+    /**
+     * Retrieves all donors associated with a specific blood bank.
+     *
+     * @param bloodBankId The ID of the blood bank.
+     * @return A list of all donors associated with the blood bank.
+     */
+    @GetMapping("/viewDonors/{bloodBankId}")
+    public ResponseEntity<List<Donor>> viewAllDonors(@PathVariable int bloodBankId){
+        return new ResponseEntity<>(bloodBankService.getAllDonors(bloodBankId),HttpStatus.OK);
+    }
+
+
+    /**
+     * Retrieves Map of blood group and their quantity associated with a specific blood bank.
+     *
+     * @param id The ID of the blood bank.
+     * @return A list of all donors associated with the blood bank
+     */
+    @GetMapping("/viewBloodGroupQuantity/{id}")
+    public ResponseEntity<Map<BloodGroup,Integer>> viewBloodGroupAndQuantity(@PathVariable int id){
+        return new ResponseEntity<>(bloodBankService.viewBloodGroupAndQuantity(id),HttpStatus.OK);
     }
 
 }
--- src/main/java/com/yp/BloodBankApplication/Services/BloodBankService.java
+++ src/main/java/com/yp/BloodBankApplication/Services/BloodBankService.java
@@ -1,6 +1,7 @@
 package com.yp.BloodBankApplication.Services;
 
 import com.yp.BloodBankApplication.Entity.BloodBank;
+import com.yp.BloodBankApplication.Entity.Donor;
 import com.yp.BloodBankApplication.Enums.BloodGroup;
 import com.yp.BloodBankApplication.Exception.BloodBankNotFoundException;
 import com.yp.BloodBankApplication.Repository.BloodBankRepository;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 import java.util.stream.Stream;
 
+/**
+ * Service class for managing blood banks and their operations.
+ */
 @Service
 public class BloodBankService {
 
@@ -18,6 +22,12 @@ public class BloodBankService {
     private BloodBankRepository bloodBankRepository;
 
 
+    /**
+     * Registers a new blood bank.
+     *
+     * @param bloodBankRequest The details of the blood bank to be registered.
+     * @return The registered blood bank.
+     */
     public BloodBank registerBloodBank(BloodBankRequest bloodBankRequest){
         List<BloodGroup> bloodGroups = Stream.of(BloodGroup.values()).toList();
         Map<BloodGroup,Integer> bloodGroupIntegerMap = new HashMap<>();
@@ -28,6 +38,14 @@ public class BloodBankService {
         return bloodBankRepository.save(bloodBank);
     }
 
+    /**
+     * Updates an existing blood bank.
+     *
+     * @param bloodBankRequest The updated details of the blood bank.
+     * @return The updated blood bank.
+     * @throws BloodBankNotFoundException If the blood bank is not found.
+     */
+
     public BloodBank updateBloodBank(BloodBankRequest bloodBankRequest){
         BloodBank bank = bloodBankRepository.findById(bloodBankRequest.getBloodBankId()).orElse(null);
         if(bank != null){
@@ -36,6 +54,82 @@ public class BloodBankService {
             bank.setPhoneNumber(bloodBankRequest.getPhoneNo());
             bank.setMailAddress(bloodBankRequest.getMailAddress());
             return bloodBankRepository.save(bank);
+        }
+        throw new BloodBankNotFoundException("Blood Bank Not Found");
+    }
+
+    /**
+     * Retrieves a blood bank by its ID.
+     *
+     * @param id The ID of the blood bank to retrieve.
+     * @return The blood bank with the specified ID.
+     * @throws BloodBankNotFoundException If the blood bank is not found.
+     */
+
+    public BloodBank viewBloodBank(int id){
+        Optional<BloodBank> bank = bloodBankRepository.findById(id);
+        if(bank.isPresent()){
+            return bank.get();
+        }
+        throw new BloodBankNotFoundException("Blood Bank Not Found");
+    }
+
+    /**
+     * Retrieves all blood banks.
+     *
+     * @return A list of all blood banks.
+     */
+
+    public List<BloodBank> viewAllBloodBanks(){
+        return bloodBankRepository.findAll();
+    }
+
+
+    /**
+     * Deletes a blood bank by its ID.
+     *
+     * @param id The ID of the blood bank to delete.
+     * @return A message indicating the deletion status.
+     * @throws BloodBankNotFoundException If the blood bank is not found.
+     */
+    public String deleteBloodBank(int id){
+        Optional<BloodBank> bloodBank = bloodBankRepository.findById(id);
+        if(bloodBank.isPresent()){
+            bloodBankRepository.deleteById(id);
+            return "Blood Bank deleted";
+        }
+        throw new BloodBankNotFoundException("Blood Bank Not Found");
+    }
+
+    /**
+     * Retrieves all donors associated with a specific blood bank.
+     *
+     * @param bloodBankid The ID of the blood bank.
+     * @return A list of all donors associated with the blood bank.
+     * @throws BloodBankNotFoundException If the blood bank is not found.
+     */
+
+    public List<Donor> getAllDonors(int bloodBankid){
+        Optional<BloodBank> bloodBank = bloodBankRepository.findById(bloodBankid);
+        if(bloodBank.isPresent()){
+            return bloodBank.get().getDonorList();
+        }
+        throw new BloodBankNotFoundException("Blood Bank Not Found");
+    }
+
+    /**
+     * Retrieves the blood groups and their quantities available in a blood bank.
+     *
+     * @param bloodBankId The ID of the blood bank.
+     * @return A map containing the blood groups and their corresponding quantities.
+     * @throws BloodBankNotFoundException If the blood bank is not found.
+     */
+
+
+    public Map<BloodGroup,Integer> viewBloodGroupAndQuantity(int bloodBankId){
+        Optional<BloodBank> bloodBank = bloodBankRepository.findById(bloodBankId);
+        if(bloodBank.isPresent()){
+            return bloodBank.get().getBloodGroups();
         }
         throw new BloodBankNotFoundException("Blood Bank Not Found");
     }
